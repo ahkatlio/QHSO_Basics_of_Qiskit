de617554971e1122583cfdc534dcc59bf16d1d74
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2023-09-26 13:02:22 +0600

    :eyes:

--- Books/Fundamentals/Basic_Gates/Algorithms/oracle_box.py
+++ Books/Fundamentals/Basic_Gates/Algorithms/oracle_box.py
@@ -1,14 +1,10 @@
-from qiskit import QuantumCircuit
-
-def oracle_box(hidden_code):
+def oracle_box(qc, hidden_code):
 
     # Get the length of the binary string
     length = len(hidden_code)
 
     # Create a quantum circuit with the given length
-    qc = QuantumCircuit(length+1, length+1)
     for i in range(length):
         if hidden_code[i] == '1':
             qc.cx(i, length)
-    return qc
 
